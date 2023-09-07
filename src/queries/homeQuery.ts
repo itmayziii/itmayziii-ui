1a787d55e6bc03e5a84023b05286e003dc8ff08a
Tommy May <tommymay37@gmail.com>
2023-09-07 16:11:36 -0400

    feat(headshot component): now serving many different image formats and sizes based on DPR
    device pixel ratio - https://web.dev/learn/design/picture-element

--- src/queries/homeQuery.ts
+++ src/queries/homeQuery.ts
@@ -1,54 +1,95 @@
 import gql from 'graphql-tag'
 
 export default gql`
-query Home {
-  UserInfo {
-    fullName
-    description
-    titles {
-      title
-    }
-    socials {
-      icon
-      link
-      label
-    }
-  }
-  Home {
-    createdAt
-    updatedAt
-    whatIDo {
-      title
-      sections {
-        icon
+  query Home {
+    UserInfo {
+      fullName
+      description
+      titles {
         title
-        description
+      }
+      socials {
+        icon
+        link
+        label
       }
     }
-    skills {
-      title
+    Home {
+      createdAt
+      updatedAt
+      whatIDo {
+        title
+        sections {
+          icon
+          title
+          description
+        }
+      }
       skills {
-        name
-        proficiency
+        title
+        skills {
+          name
+          proficiency
+        }
       }
     }
-  }
-  Nav {
-    links {
-      label
-      link
+    Nav {
+      links {
+        label
+        link
+      }
     }
-  }
-  Headshots(where: { name: { equals: "Professional" } }) {
-    docs {
-      alt
-      sizes {
-        mobile {
-          filename
-          url
+    Headshots(where: { name: { equals: "Professional" } }) {
+      docs {
+        alt
+        sizes {
+          mobile {
+            url
+          }
+          mobileWebp {
+            url
+          }
+          mobileAvif {
+            url
+          }
+          mobile1_5x {
+            url
+          }
+          mobileAvif1_5x {
+            url
+          }
+          mobileWebp1_5x {
+            url
+          }
+          mobile2x {
+            url
+          }
+          mobileAvif2x {
+            url
+          }
+          mobileWebp2x {
+            url
+          }
+          mobile3x {
+            url
+          }
+          mobileAvif3x {
+            url
+          }
+          mobileWebp3x {
+            url
+          }
+          mobile4x {
+            url
+          }
+          mobileAvif4x {
+            url
+          }
+          mobileWebp4x {
+            url
+          }
         }
       }
     }
   }
-}
 `
--- src/utilities.ts
+++ src/utilities.ts
@@ -1,3 +1,26 @@
 export function absoluteUrl (relativePath: string, baseUrl: string | URL): string {
   return (new URL(relativePath, baseUrl)).toString()
 }
+
+export function mediaUrl (url: string): string {
+  if (url.startsWith('/')) {
+    return `${import.meta.env.MEDIA_URL}${url}`
+  }
+  if (url.startsWith('http')) {
+    return url
+  }
+
+  // Being somewhat forgiving in case the starting "/" is missing
+  return `${import.meta.env.MEDIA_URL}/${url}`
+}
+
+interface Densities {
+  1?: string
+  '1.5'?: string
+  2?: string
+  3?: string
+  4?: string
+}
+export function densitySrcset (densities: Densities) {
+  return Object.keys(densities).map(k => `${densities[k]} ${k}x`).join(', ')
+}
